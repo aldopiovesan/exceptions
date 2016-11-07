54832188cc258e8cff9b2ba3973a7d4b5016d6ab
tss <tss@debian-tss24>
2016-11-07 16:04:13 +0100

    aggiunto input utente

--- src/lezione1/Main.java
+++ src/lezione1/Main.java
@@ -5,6 +5,8 @@
  */
 package lezione1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author tss
@@ -14,5 +16,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("primo software started...");
         
+        
+        String frase = JOptionPane.showInputDialog("inserisci frase da stampare");
+        System.out.println(frase);
     }
 }
