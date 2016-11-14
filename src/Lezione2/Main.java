62da5882fb6dcc376b9389a686326345079e738c
tss <tss@debian-tss24>
2016-11-14 12:33:03 +0100

    creato package lez 2

--- src/Lezione2/Main.java
+++ src/Lezione2/Main.java
@@ -11,27 +11,47 @@ package Lezione2;
  * @author tss
  */
 public class Main {
-    
+
+    //variabile statica
+    private int colore;
+
+    //variabile d'istanza
+    private int carattere;
+
     /**
      * da questo metodo partirà l'esecuzione
-     * @param args 
+     *
+     * @param args
      */
-    
     public static void main(String[] args) {
-        
-        
-        
+
         // commento su una sola riga
-        
-        
         /*
         
         commento su piu righe
-        */
-        System.out.println("Start esempio main"); 
-        
-        
-        
+         */
+        int totale = 10;
+        String positivo;
+
+        if (totale > 0) {
+
+            //variabile locale
+            int x = 5;
+            positivo = "il totale è positivo";
+
+        } else {
+            int x = 10;
+            positivo = "il toale è negativo";
+
+        }
+
+        //if ternario
+        positivo = (totale > 0) ? "il totale è positivo" : "il totale è negativo";
+
+        totale += 20;
+
+        System.out.println("totale " + totale);
+
     }
-    
+
 }
