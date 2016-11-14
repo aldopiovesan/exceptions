e1b3a383f0c1f2c13e8e07f794c53aefb81e8889
tss <tss@debian-tss24>
2016-11-14 11:12:29 +0100

    creato package lez 2

---
+++ src/Lezione2/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lezione2;
+
+/**
+ *
+ *
+ * @author tss
+ */
+public class Main {
+    
+    /**
+     * da questo metodo partirà l'esecuzione
+     * @param args 
+     */
+    
+    public static void main(String[] args) {
+        
+        
+        
+        // commento su una sola riga
+        
+        
+        /*
+        
+        commento su piu righe
+        */
+        System.out.println("Start esempio main"); 
+        
+        
+        
+    }
+    
+}
